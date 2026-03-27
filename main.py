41d67369575ac3d3d8ad7b8dbdb4adce0604ce65
KM <kmalarcon.freelancer@gmail.com>
2026-03-27 22:39:55 +0800

    improved deposit and withdraw methods

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ class Account:
             return "Insufficient funds"
         else:
             self.balance -= amount
-            return f"New balance: {self.balance}"
+            return self.balance
 
     def check_balance(self):
-        return f"Current balance: {self.balance}"
+        return self.balance
 
 new_account = Account("John Doe", 1000)
-print(new_account.check_balance())
-print(f"Deposit result: {new_account.deposit(500)}")
-print(new_account.withdraw(200))
-print(new_account.withdraw(1500))
-print(f"Deposit result: {new_account.deposit(-500)}")
-print(new_account.check_balance())
+print(f"Balance: {new_account.check_balance()}")
+print(f"Deposit: {new_account.deposit(500)}")
+print(f"Withdraw: {new_account.withdraw(200)}")
+print(f"Withdraw: {new_account.withdraw(1500)}")
+print(f"Deposit: {new_account.deposit(-500)}")
+print(f"Balance: {new_account.check_balance()}")
